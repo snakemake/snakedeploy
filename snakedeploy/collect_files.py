02b784ee659098e85bbf40449df47f6fc6967f56
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-01-18 12:55:54 +0100

    skip entry if no files were found, issuing a warning.

--- snakedeploy/collect_files.py
+++ snakedeploy/collect_files.py
@@ -1,6 +1,7 @@
 import sys
 from glob import glob
 import re
+import logging
 
 from snakedeploy.exceptions import UserError
 
@@ -11,8 +12,12 @@ def collect_files(input_pattern: str, glob_pattern: str):
         match = input_regex.match(item)
         if match is None:
             raise UserError(f"input pattern {input_pattern} does not match {item}")
-        files = sorted(glob(glob_pattern.format(**{key: autoconvert(value) for key, value in match.groupdict().items()})))
-        print(item, *files, sep="\t")
+        pattern = glob_pattern.format(**{key: autoconvert(value) for key, value in match.groupdict().items()})
+        files = sorted(glob(pattern))
+        if files:
+            print(item, *files, sep="\t")
+        else:
+            logging.warning(f"Skipped {item} because no files were found with pattern {pattern}.")
     
 
 def autoconvert(value):
