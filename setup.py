84d2601283972dcab558be6d06a50fef7a20386e
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-04-23 18:31:44 +0200

    min python

--- setup.py
+++ setup.py
@@ -101,7 +101,6 @@ if __name__ == "__main__":
             "Natural Language :: English",
             "Operating System :: Unix",
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
