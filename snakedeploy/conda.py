e9adca76a881f7a9d4781add4b1ef03aa2491191
Johannes Köster <johannes.koester@uni-due.de>
2022-10-27 08:30:11 +0200

    fix: shuffle conda envs before updating (this ensures that running this in a fallible github action cron job still leads to updating all envs from time to time)

--- snakedeploy/conda.py
+++ snakedeploy/conda.py
@@ -9,6 +9,7 @@ import re
 from glob import glob
 from itertools import chain
 from urllib3.util.retry import Retry
+import random
 
 from packaging import version as packaging_version
 import yaml
@@ -84,7 +85,9 @@ class CondaEnvProcessor:
                 ),
             )
             repo = g.get_repo(os.environ["GITHUB_REPOSITORY"]) if create_prs else None
-        conda_envs = list(chain.from_iterable(map(glob, conda_env_paths)))
+        conda_envs = random.shuffle(
+            list(chain.from_iterable(map(glob, conda_env_paths)))
+        )
         if not conda_envs:
             logger.info(
                 f"No conda envs found at given paths: {', '.join(conda_env_paths)}"
