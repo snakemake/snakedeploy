6e6fd1827e99cb691ff477e6bfdb57392ec0901f
jakevc <jake.vancampen7@gmail.com>
2025-05-18 15:59:58 -0700

    explicit

--- src/snakedeploy/__init__.py
+++ src/snakedeploy/__init__.py
@@ -3,7 +3,7 @@ __copyright__ = "Copyright 2020-2021, Vanessa Sochat"
 __license__ = "MPL 2.0"
 
 try:
-    from ._version import version as __version__
+    from snakedeploy._version import version as __version__
 except ImportError:
     __version__ = "0.0.0"  # Fallback when not installed properly
 
