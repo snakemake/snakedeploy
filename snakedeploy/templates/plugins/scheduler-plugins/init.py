2697ec2f8c399f69f7e0ba2703beda9c043cc118
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2026-03-19 09:32:22 +0100

    Add missing type

--- snakedeploy/templates/plugins/scheduler-plugins/init.py
+++ snakedeploy/templates/plugins/scheduler-plugins/init.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Mapping, Optional, Sequence, Union
+from typing import Dict, Mapping, Optional, Sequence, Union
 
 from snakemake_interface_scheduler_plugins.settings import SchedulerSettingsBase
 from snakemake_interface_scheduler_plugins.base import SchedulerBase
