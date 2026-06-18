c6a03b68f401624eccfa01b420907f1031866541
Felix Wiegand <fxwiegand@wgdnet.de>
2026-06-18 06:38:12 +0200

    fix: Fix --version

--- snakedeploy/__init__.py
+++ snakedeploy/__init__.py
@@ -1,3 +1,10 @@
+from importlib.metadata import PackageNotFoundError, version
+
 __author__ = "Vanessa Sochat"
 __copyright__ = "Copyright 2020-2021, Vanessa Sochat"
 __license__ = "MPL 2.0"
+
+try:
+    __version__ = version("snakedeploy")
+except PackageNotFoundError:
+    __version__ = "unknown"
